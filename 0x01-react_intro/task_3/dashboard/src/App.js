64e8d092474ceb5ea73e0bdaecbb033f8f1aa8f2
Eskiasy <eskiasy@gmail.com>
2023-08-23 18:32:46 +0300

    task 3 test

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import logo from './holberton-logo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+
+
+function App() {
+    return (
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} alt="Holberton Logo" />
+              <h1>School dashboard</h1>
+          </header>
+
+          <body className="App-body">
+              <p>Login to access the full dashboard</p>
+              <label htmlFor='email'>
+                  Email: <input type="email" id="email" name="email" />
+              </label>
+              <label htmlFor='password'>
+                  Password: <input type="password" id="password" name="password"></input>
+              </label>
+              <button>OK</button>
+          </body>
+          <footer className="App-footer">
+              <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+          </footer>
+      </div>
+    );
+}
+
+export default App;
