c3dfc3e5722f20cab326e5fd2506a68b117d164f
exuszee <exuszee@gmail.com>
2023-08-23 10:45:49 +0300

    task 2

--- 0x01-react_intro/task_1/dashboard/src/index.js
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -8,18 +8,14 @@ import Notifications from './Notifications';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div className="root-notifications">
+      <Notifications />
+    </div>
     <App />
   </React.StrictMode>
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Notifications />
-  </React.StrictMode>,
-  document.getElementById('root-notifications')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -5,25 +5,26 @@ import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
     return (
-        <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="Holberton Logo" />
-        <h1>School dashboard</h1>
-      </header>
-      <body className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>
-          Email: <input type="email" id="email" name="email" />
-        </label>
-        <label htmlFor='password'>
-          Password: <input type="password" id="password" name="password"></input>
-        </label>
-        <button>OK</button>        
-      </body>
-      <footer className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </footer>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} alt="Holberton Logo" />
+              <h1>School dashboard</h1>
+          </header>
+
+          <body className="App-body">
+              <p>Login to access the full dashboard</p>
+              <label htmlFor='email'>
+                  Email: <input type="email" id="email" name="email" />
+              </label>
+              <label htmlFor='password'>
+                  Password: <input type="password" id="password" name="password"></input>
+              </label>
+              <button>OK</button>
+          </body>
+          <footer className="App-footer">
+              <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+          </footer>
+      </div>
     );
 }
 
