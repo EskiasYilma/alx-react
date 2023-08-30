2211d7229b02ca9645f375a379c54c711b7255e3
exuszee <exuszee@gmail.com>
2023-08-30 18:10:07 +0300

    3

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+import BodySection from './BodySection';
+
+describe('BodySectionWithMarginBottom Component', () => {
+    it('render margin bottom to child', () => {
+        const wrapper = shallow(<BodySectionWithMarginBottom title='test title' />);
+
+        expect(wrapper.find(BodySection).html()).toEqual(
+            '<div class="bodySection"><h2>test title</h2></div>'
+        );
+    });
+});
