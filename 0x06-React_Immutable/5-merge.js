5ea7bc1dac7c07c05d5e80380049b04f18536b5b
exuszee <exuszee@gmail.com>
2023-09-13 15:59:58 +0300

    4 fix

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -7,7 +7,7 @@ export function concatElements(page1, page2) {
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  const merged = map1.merge(map2);
-  return List(merged.values());
+  const merged = map1.concat(map2);
+  return merged;
 }
 
