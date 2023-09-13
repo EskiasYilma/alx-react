71be2fbb0d7828b36a0f4373b1644715416d37a4
exuszee <exuszee@gmail.com>
2023-09-13 15:06:47 +0300

    0-1 fix

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,17 +1,5 @@
+import { getIn, fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  if (typeof object !== 'object' || array.length === 0) {
-    return undefined;
-  }
-
-  const current = array[0];
-
-  if (object.hasOwnProperty(current)) {
-    if (array.length > 1) {
-      return accessImmutableObject(object[current], array.slice(1));
-    } else {
-      return object[current];
-    }
-  } else {
-    return undefined;
-  }
+  return getIn(fromJS(object), array);
 }
