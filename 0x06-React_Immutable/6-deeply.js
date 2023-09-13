dc30501eb2c1410b7a2add9479eae3c74d7f4b59
exuszee <exuszee@gmail.com>
2023-09-13 17:28:11 +0300

    6 fix

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,5 @@
 import { Map } from 'immutable';
 
 export function mergeDeeplyElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-  const merged = mergeDeep(map1, map2);
-  return List(merged.values());
+  return Map(page1).mergeDeep(Map(page2));
 }
