38824029d28d7e52ecb9f07d3ea16c017bde3cb2
exuszee <exuszee@gmail.com>
2023-09-18 17:31:42 +0300

    1

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,18 @@
+import { normalize, schema } from 'normalizr';
+import notificationsJson from '../../../../notifications.json';
+const userSchema = new schema.Entity('users');
+const messageSchema = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notificationSchema = new schema.Entity('notification', {
+  author: userSchema,
+  context: messageSchema,
+});
+
+const normalizeNotifications = normalize(notificationsJson, [notificationSchema]);
+
+export default function getAllNotificationsByUser(userId) {
+  return notificationsJson
+    .filter(notification => notification.author.id === userId)
+    .map(notification => notification.context);
+}
+
+export { normalizeNotifications }
